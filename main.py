f4a3e902a5cb18b39d10edb641cfe082510e8c4c
Javier Escobar <jescobar7420@gmail.com>
2022-06-20 22:10:09 -0400

    Comments and translate to english

--- main.py
+++ main.py
@@ -1,153 +1,165 @@
+"""
+    Investigación de Operaciones - PUCV
+    Tarea 2
+    Autores: Javier Escobar - Diego Riveros
+"""
+
+# Librerias
 import random
 from math import e
 
-ARCHIVO1 = "QAP_sko56_04_n.txt"
-ARCHIVO2 = "QAP_sko100_04_n.txt"
-T_TERMINO = 0.2
+# Constantes
+FILE1 = "QAP_sko56_04_n.txt"
+FILE2 = "QAP_sko100_04_n.txt"
+T_END = 0.2
 
 
-def leer_archivo(archivo):
+def read_file(file):
     list = []
     try:
-        with open(archivo, "r") as a:
+        with open(file, "r") as a:
             list = a.read().split("\n")
     except FileExistsError:
         print("Archivo no encontrado.")
     return list
+    
+
+def get_n_stores(data):
+    # En la posición 0 de la lista 'data' se encuentra el número de locales
+    return int(data[0])
 
 
-def dividir_tam_locales(datos):
-    tam_locales = []
-    tam_locales = datos[1].split(",")
-    tam_locales = pasar_a_entero(tam_locales)
-    return tam_locales
+def get_size_stores(data):
+    # En la posición 1 de la lista 'data' se encuentra el tamanio de los locales
+    size_stores = []
+    size_stores = data[1].split(",")
+    size_stores = pass_an_int(size_stores)
+    return size_stores
 
 
-def dividir_cant_locales(datos):
-    return int(datos[0])
+def pass_an_int(list):
+    # Transforma la lista de string a integer
+    return [int(x) for x in list]
 
 
-def pasar_a_entero(lista):
-    return [int(x) for x in lista]
+def get_neighborhood(data):
+    list_neighborhood = []
+    # Crea una matriz/lista de listas con el vecindario
+    for i in range(2, len(data)-1):
+        row = data[i].split(",")
+        row = pass_an_int(row)
+        list_neighborhood.append(row)
+    return list_neighborhood
 
 
-def dividir_personas_esperadas(datos):
-    list_cant_personas = []
-    i = 2 # Donde comienza los datos de cantidad de gente esperada en los locales
-
-    for i in range(2, len(datos)-1):
-        row = datos[i].split(",")
-        row = pasar_a_entero(row)
-        list_cant_personas.append(row)
-
-    return list_cant_personas
-
-
-def calcular_distancia(i, j, tam_locales):
-    suma = 0
+def calculate_distance(i, j, size_stores):
+    # Calcula la distancia que existe entre un local A al local B
+    sum = 0
     for k in range(i+1, j):
-        suma += tam_locales[k]
-    suma += (tam_locales[i]/2) + (tam_locales[j]/2)
-    return suma
+        sum += size_stores[k]
+    sum += (size_stores[i]/2) + (size_stores[j]/2)
+    return sum
 
 
-def calcular_esfuerzo(n, tam_locales, lista_cant_personas):
-    suma = 0
+def calculate_efford(n, size_stores, list_neighborhood):
+    # Calcula el esfuerzo del cliente en caminar de un puesto a otro
+    sum = 0
     for i in range(n-1):
         for j in range(i+1, n):
-            distancia = calcular_distancia(i, j, tam_locales)
-            suma += distancia + lista_cant_personas[i][j]
-    return suma
+            distance = calculate_distance(i, j, size_stores)
+            sum += distance + list_neighborhood[i][j]
+    return sum
 
 
-def criterio_aceptacion(mejor_puntaje, nuevo_puntaje, T):
-    ds = nuevo_puntaje - mejor_puntaje
-
+def acceptance_requirements(best_score, new_score, temperature):
+    ds = new_score - best_score
+    # En caso de que el nuevo puntaje sea mejor que el actual, se acepta directamente
     if ds < 0:
         return True
-    if T==0:
+    if temperature==0:
         return False
 
-    p = e**(-ds/T)
+    # En caso contrario, se usa el criterio de metrópolis
+    p = e**(-ds/temperature)
     random_prob = random.random()
 
     if random_prob < p:
         return True
-
     return False
 
 
-def swap(lista_cant_personas, tam_locales, n):
-    a = None
-    b = None
-    while True:
-        a = random.randint(0, n-1)
-        b = random.randint(0, n-1)
-        if a != b:
-            break
+def swap(list_neighborhood, size_stores, n_stores):
+    a = 0
+    b = 0
+    # Intercambiar dos locales aleatoriamente
+    while a == b:
+        a = random.randint(0, n_stores-1)
+        b = random.randint(0, n_stores-1)
     
     # Swap en los tamanios de los locales
-    copy_tam = tam_locales[a]
-    tam_locales[a] = tam_locales[b]
-    tam_locales[b] = copy_tam
+    copy_tam = size_stores[a]
+    size_stores[a] = size_stores[b]
+    size_stores[b] = copy_tam
     
     # Swap en la lista de personas esperadas
-    for i in range(n):
-        copy_pers = lista_cant_personas[i][a]
-        lista_cant_personas[i][a] = lista_cant_personas[i][b]
-        lista_cant_personas[i][b] = copy_pers
+    for i in range(n_stores):
+        copy_pers = list_neighborhood[i][a]
+        list_neighborhood[i][a] = list_neighborhood[i][b]
+        list_neighborhood[i][b] = copy_pers
 
 
-def criterio_termino(T, T_TERMINO):
-    if T < T_TERMINO:
+def ending_requirement(temperature):
+    # Criterio de termino de las iteraciones
+    if temperature < T_END:
         return False
     return True
             
 
 def main():
-    # Parametros
-    alfa = 0.9
-    T = 15000
-
-    input_usuario = None
-    datos = None
+    input_user = None
+    data = None
 
     # Elegir instancia
-    while input_usuario != 1 and input_usuario != 2:
-        input_usuario = input('Elegir instancia. [1/2]: ')
-        if input_usuario == '1':
-            input_usuario = 1
-            datos = leer_archivo(ARCHIVO1)
-        elif input_usuario == '2':
-            input_usuario = 2
-            datos = leer_archivo(ARCHIVO2)
+    while input_user != 1 and input_user != 2:
+        input_user = input('Elegir instancia. [1/2]: ')
+        if input_user == '1':
+            input_user = 1
+            data = read_file(FILE1)
+        elif input_user == '2':
+            input_user = 2
+            data = read_file(FILE2)
 
     # Parametros
-    n = dividir_cant_locales(datos) # Cantidad de locales
-    tam_locales = dividir_tam_locales(datos) # Tamanio de los locales
-    lista_cant_personas = dividir_personas_esperadas(datos) # Cantidad de gente esperada por local
+    n_stores = get_n_stores(data) # Cantidad de locales
+    size_stores = get_size_stores(data) # Tamanio de los locales
+    list_neighborhood = get_neighborhood(data) # Cantidad de gente esperada por local
+    alfa = 0.9
+    temperature = 15000
     
     # Solucion inicial
-    mejor_vecindario = lista_cant_personas
-    mejor_tam_locales = tam_locales
-    mejor_puntaje = calcular_esfuerzo(n, mejor_tam_locales, mejor_vecindario)
+    best_neighborhood = list_neighborhood
+    best_size_stores = size_stores
+    best_score = calculate_efford(n_stores, best_size_stores, best_neighborhood)
 
-    while criterio_termino(T, T_TERMINO):
-        nuevo_vecindario = mejor_vecindario
-        nuevo_tam_locales = mejor_tam_locales
-        swap(nuevo_vecindario, nuevo_tam_locales, n)
-        nuevo_puntaje = calcular_esfuerzo(n, nuevo_tam_locales, nuevo_vecindario)
+    # Hacer iteraciones hasta que se cumpla el criterio de termino
+    while ending_requirement(temperature):
+        # Crear nueva solución con swap de la solución anterior
+        new_neighborhood = best_neighborhood
+        new_size_stores = best_size_stores
+        swap(new_neighborhood, new_size_stores, n_stores)
+        new_score = calculate_efford(n_stores, new_size_stores, new_neighborhood)
         
-        if criterio_aceptacion(mejor_puntaje, nuevo_puntaje, T):
-            mejor_puntaje = nuevo_puntaje
-            mejor_vecindario = nuevo_vecindario
-            mejor_tam_locales = nuevo_tam_locales
+        # Comparar puntajes obtenidos
+        if acceptance_requirements(best_score, new_score, temperature):
+            best_score = new_score
+            best_neighborhood = new_neighborhood
+            best_size_stores = new_size_stores
         
-        T *= alfa
-        print("Temperatura actual: {}".format(T))
-    
-    print("Mejor puntaje: {}".format(mejor_puntaje))
-    print("Mejor vecindario:\n{}\n{}".format(mejor_tam_locales, mejor_vecindario))
+        # Disminuir temperatura por cada iteración
+        temperature *= alfa
+        print("Temperatura actual: {}".format(temperature))
+    print("Mejor puntaje: {}".format(best_score))
+    print("Mejor vecindario:\n{}\n{}".format(best_size_stores, best_neighborhood))
     
 
 if __name__ == '__main__':
